f3c83b4e838fac6589aa82296e15048e99c6eb41
Bastian Thomas <bastian@ckthomas.de>
2021-12-31 17:55:40 +0100

    Abhängigkeiten hinterlegt und erste Testmethode geschrieben.

---
+++ src/test/kotlin/de/bcthms/games/mastermind/GameTest.kt
@@ -0,0 +1,15 @@
+package de.bcthms.games.mastermind
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class GameTest {
+
+    @Test
+    fun helloTest() {
+        println("Frohes neues Jahr :-)")
+        val sum = 45 + 25
+        Assertions.assertEquals(70, sum)
+    }
+
+}
